2d4df662b2dfe72609dec3d32bd29b6744f13c77
Austin DeHart <m609542@yahoo.com>
2021-12-05 12:06:48 -0800

    extract lop to method

--- deep_q_network.py
+++ deep_q_network.py
@@ -121,96 +121,92 @@ def trainNetwork(s, readout, h_fc1, sess):
     epsilon = INITIAL_EPSILON
     t = 0
     while "flappy bird" != "angry bird":
-        # choose an action epsilon greedily
-        readout_t = readout.eval(feed_dict={s : [s_t]})[0]
-        a_t = np.zeros([ACTIONS])
-        action_index = 0
-        if t % FRAME_PER_ACTION == 0:
-            if random.random() <= epsilon:
-                print("----------Random Action----------")
-                action_index = random.randrange(ACTIONS)
-                a_t[random.randrange(ACTIONS)] = random.random()*2-1
+        train_loop(D, a, epsilon, game_state, readout, s, s_t, saver, sess, t, train_step, y)
+
+
+def train_loop(D, a, epsilon, game_state, readout, s, s_t, saver, sess, t, train_step, y):
+    # choose an action epsilon greedily
+    readout_t = readout.eval(feed_dict={s: [s_t]})[0]
+    a_t = np.zeros([ACTIONS])
+    action_index = 0
+    if t % FRAME_PER_ACTION == 0:
+        if random.random() <= epsilon:
+            print("----------Random Action----------")
+            action_index = random.randrange(ACTIONS)
+            a_t[random.randrange(ACTIONS)] = random.random() * 2 - 1
+        else:
+            if readout_t[0] < 0:
+                a_t[0] = 0
             else:
-                if readout_t[0] < 0:
-                    a_t[0] = 0
-                else:
-                    a_t[0] = 1
-                if a_t[0] == 0:
-                    a_t[1] = readout_t[1]*100
-        # print(f"State: {s} | Readout: {readout_t}")
-        #         action_index = np.argmax(readout_t)
-                # a_t[action_index] = 1
-                # if action_index == 1:
-                #     a_t[1] = random.random()*2 - 1
-        else:
-            a_t[0] = 1  # do nothing
+                a_t[0] = 1
+            if a_t[0] == 0:
+                a_t[1] = readout_t[1] * 100
+    # print(f"State: {s} | Readout: {readout_t}")
+    #         action_index = np.argmax(readout_t)
+    # a_t[action_index] = 1
+    # if action_index == 1:
+    #     a_t[1] = random.random()*2 - 1
+    else:
+        a_t[0] = 1  # do nothing
+    # print(f"Action: {a_t}")
+    # scale down epsilon
+    if epsilon > FINAL_EPSILON and t > OBSERVE:
+        epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
+    # run the selected action and observe next state and reward
+    x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
+    x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
+    ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
+    x_t1 = np.reshape(x_t1, (80, 80, 1))
+    # s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
+    s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
+    # store the transition in D
+    D.append((s_t, a_t, r_t, s_t1, terminal))
+    if len(D) > REPLAY_MEMORY:
+        D.popleft()
+    # only train if done observing
+    if t > OBSERVE:
+        # sample a minibatch to train on
+        minibatch = random.sample(D, BATCH)
 
-        # print(f"Action: {a_t}")
-        # scale down epsilon
-        if epsilon > FINAL_EPSILON and t > OBSERVE:
-            epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
+        # get the batch variables
+        s_j_batch = [d[0] for d in minibatch]
+        a_batch = [d[1] for d in minibatch]
+        r_batch = [d[2] for d in minibatch]
+        s_j1_batch = [d[3] for d in minibatch]
 
-        # run the selected action and observe next state and reward
-        x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
-        x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
-        ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
-        x_t1 = np.reshape(x_t1, (80, 80, 1))
-        #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
-        s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
+        y_batch = []
+        readout_j1_batch = readout.eval(feed_dict={s: s_j1_batch})
+        for i in range(0, len(minibatch)):
+            terminal = minibatch[i][4]
+            # if terminal, only equals reward
+            if terminal:
+                y_batch.append(r_batch[i])
+            else:
+                y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
 
-        # store the transition in D
-        D.append((s_t, a_t, r_t, s_t1, terminal))
-        if len(D) > REPLAY_MEMORY:
-            D.popleft()
-
-        # only train if done observing
-        if t > OBSERVE:
-            # sample a minibatch to train on
-            minibatch = random.sample(D, BATCH)
-
-            # get the batch variables
-            s_j_batch = [d[0] for d in minibatch]
-            a_batch = [d[1] for d in minibatch]
-            r_batch = [d[2] for d in minibatch]
-            s_j1_batch = [d[3] for d in minibatch]
-
-            y_batch = []
-            readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
-            for i in range(0, len(minibatch)):
-                terminal = minibatch[i][4]
-                # if terminal, only equals reward
-                if terminal:
-                    y_batch.append(r_batch[i])
-                else:
-                    y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
-
-            # perform gradient step
-            train_step.run(feed_dict = {
-                y : y_batch,
-                a : a_batch,
-                s : s_j_batch}
-            )
-
-        # update the old values
-        s_t = s_t1
-        t += 1
-
-        # save progress every 10000 iterations
-        if t % 1000 == 0:
-            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step = t)
-
-        # print info
-        state = ""
-        if t <= OBSERVE:
-            state = "observe"
-        elif t > OBSERVE and t <= OBSERVE + EXPLORE:
-            state = "explore"
-        else:
-            state = "train"
-
-        print("TIMESTEP", t, "/ STATE", state,
-              "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t,
-              "/ Q_MAX %e" % np.max(readout_t))
+        # perform gradient step
+        train_step.run(feed_dict={
+            y: y_batch,
+            a: a_batch,
+            s: s_j_batch}
+        )
+    # update the old values
+    s_t = s_t1
+    t += 1
+    # save progress every 10000 iterations
+    if t % 1000 == 0:
+        saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step=t)
+    # print info
+    state = ""
+    if t <= OBSERVE:
+        state = "observe"
+    elif t > OBSERVE and t <= OBSERVE + EXPLORE:
+        state = "explore"
+    else:
+        state = "train"
+    print("TIMESTEP", t, "/ STATE", state,
+          "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t,
+          "/ Q_MAX %e" % np.max(readout_t))
 
 
 def playGame():
